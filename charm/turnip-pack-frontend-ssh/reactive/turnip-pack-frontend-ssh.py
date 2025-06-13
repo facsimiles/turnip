1d95edd827404db925eec52df6525901b5b71ccc
Guruprasad Lakshmi Narayanan <guruprasad.ln@canonical.com>
2025-06-13 11:28:21 +0530

    charm/*: Handle relation departures properly
    This is required for the configuration to be set up properly when a
    relationship is re-joined after a departure.

--- charm/turnip-pack-frontend-ssh/reactive/turnip-pack-frontend-ssh.py
+++ charm/turnip-pack-frontend-ssh/reactive/turnip-pack-frontend-ssh.py
@@ -42,6 +42,16 @@ def turnip_pack_virt_available():
         )
 
 
+@when_not("turnip-pack-virt.available")
+@when("turnip.services.pack-virt")
+def turnip_pack_virt_unavailable():
+    clear_flag("turnip.services.pack-virt")
+    clear_flag("turnip.configured")
+    status.blocked(
+        "turnip-pack-virt must be related to the http interface"
+    )
+
+
 @when(
     "turnip.installed",
     "turnip.services.pack-virt",
